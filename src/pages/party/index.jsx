6591b87d0df84420409580c65175a823dd24863c
MadBoy <73926361+madboy482@users.noreply.github.com>
2025-03-26 23:42:56 +0530

    .

---
+++ src/pages/party/index.jsx
@@ -0,0 +1,5 @@
+import AddressPage from './AddressPage';
+import CreatePartyPage from './CreatePartyPage';
+import PartiesPage from './PartiesPage';
+
+export { AddressPage, CreatePartyPage, PartiesPage };
