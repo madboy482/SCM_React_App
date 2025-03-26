09c04ac1e238d22bc521ffc4086a9750b20c46bb
MadBoy <73926361+madboy482@users.noreply.github.com>
2025-03-26 23:42:40 +0530

    .

---
+++ src/pages/dashboards/index.jsx
@@ -0,0 +1,5 @@
+import AdminDashboard from './AdminDashboard';
+import SupplierDashboard from './SupplierDashboard';
+import CustomerDashboard from './CustomerDashboard';
+
+export { AdminDashboard, SupplierDashboard, CustomerDashboard };
